728ad554859e885d3395f1b1930b256a6c4b5bbd
jpisano99 <jpisano@cisco.com>
2019-10-18 22:26:09 -0400

    First Commit

--- my_app/func_lib/push_list_to_csv.py
+++ my_app/func_lib/push_list_to_csv.py
@@ -1,9 +1,10 @@
 import csv
-import datetime
-import time
-import xlrd
 import os
 from my_app.func_lib.open_wb import open_wb
+from my_app.func_lib.xlrd_wb_to_csv import xlrd_wb_to_csv
+from my_app.func_lib.xlrd_wb_to_list import xlrd_wb_to_list
+from my_app.func_lib.push_list_to_xls import push_list_to_xls
+
 from my_app.settings import app_cfg
 
 
@@ -28,38 +29,14 @@ def push_list_to_csv(my_list, csv_output, run_dir=app_cfg['UPDATES_SUB_DIR'], tb
 if __name__ == "__main__" and __package__ is None:
     # wb, ws = open_wb(app_cfg['XLS_BOOKINGS'])
     wb, ws = open_wb(app_cfg['XLS_AS_DELIVERY_STATUS'])
+    my_csv_list = xlrd_wb_to_csv(wb, ws)
+    my_list = xlrd_wb_to_list(wb, ws)
 
-    my_csv_list = []
-    for my_row in range(ws.nrows):
-        my_csv_row = []
-        tmp_val = None
-        for my_col in range(ws.ncols):
-            my_cell = ws.cell(my_row, my_col)
+    print(len(my_csv_list))
+    print(len(my_list))
 
-            if my_cell.ctype == xlrd.XL_CELL_DATE:
-                tmp_val = datetime.datetime(*xlrd.xldate_as_tuple(my_cell.value, wb.datemode))
-                tmp_val = tmp_val.strftime('%D')
-                my_csv_row.append(tmp_val)
-
-            elif my_cell.ctype == xlrd.XL_CELL_NUMBER:
-                tmp_val = str(my_cell.value)
-                my_csv_row.append(tmp_val)
-
-            else:
-                # Strip out Unicode characters above value 127
-                # Make it all ASCII
-                cell_bytes = my_cell.value.encode('ascii', 'ignore')
-                tmp_val = cell_bytes.decode('utf-8')
-                my_csv_row.append(tmp_val)
-
-        my_csv_list.append(my_csv_row)
-
-    # print(len(my_csv_list))
-    # for row in my_csv_list:
-    #     for col in row:
-    #         print(type(col), col)
-    #     break
-
+    push_list_to_xls(my_list, 'my_xls.xlsx')
     push_list_to_csv(my_csv_list, 'my_csv.csv')
+
     exit()
 
