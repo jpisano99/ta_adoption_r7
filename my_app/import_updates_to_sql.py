cb60f0fa576a9f23ab7ec4cb31249a76e1ea20d3
jpisano99 <jpisano@cisco.com>
2019-10-20 12:34:52 -0400

    First Commit

--- my_app/import_updates_to_sql.py
+++ my_app/import_updates_to_sql.py
@@ -3,11 +3,9 @@ from my_app.settings import app_cfg
 from my_app.func_lib.push_list_to_csv import push_list_to_csv
 from my_app.func_lib.db_tools import create_tables
 from my_app.func_lib.xlrd_wb_to_csv import xlrd_wb_to_csv
-
 from my_app.func_lib.load_infile import load_infile
 
 create_tables()
-
 
 #
 # Import Delivery
@@ -20,8 +18,8 @@ for my_row in my_csv:
     my_row.insert(0, '')
     my_new_list.append(my_row)
 
-push_list_to_csv(my_new_list, 'services_csv.csv')
-load_infile('services', 'services_csv.csv')
+push_list_to_csv(my_new_list, 'csv_services.csv')
+load_infile('services', 'csv_services.csv')
 
 #
 # Import Subscriptions
@@ -34,8 +32,8 @@ for my_row in my_csv:
     my_row.insert(0, '')
     my_new_list.append(my_row)
 
-push_list_to_csv(my_new_list, 'subscriptions_csv.csv')
-load_infile('subscriptions', 'subscriptions_csv.csv')
+push_list_to_csv(my_new_list, 'csv_subscriptions.csv')
+load_infile('subscriptions', 'csv_subscriptions.csv')
 
 #
 # Import Bookings
@@ -48,5 +46,5 @@ for my_row in my_csv:
     my_row.insert(0, '')
     my_new_list.append(my_row)
 
-push_list_to_csv(my_new_list, 'bookings_csv.csv')
-load_infile('bookings', 'bookings_csv.csv')
+push_list_to_csv(my_new_list, 'csv_bookings.csv')
+load_infile('bookings', 'csv_bookings.csv')
